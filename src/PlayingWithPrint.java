b64b64c5b33f206d684a11c1a0e04c95155bfa48
Phillip Forkner <phillip.forkner@gmail.com>
2025-04-09 11:45:29 -0700

    Monday's class

---
+++ src/PlayingWithPrint.java
@@ -0,0 +1,21 @@
+// April 9, 2025
+// Phillip Forkner
+// Playing with strings and print
+/*
+
+This is a block comment
+
+ */
+
+public class PlayingWithPrint {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("My name is Phillip\\norkner");
+        System.out.println("| x\t | y\t |");
+        System.out.println("| 0\t | 0\t |");
+        System.out.println("| 1\t | 1\t |");
+        System.out.println("| 2\t | 4\t |");
+        System.out.print("Phillip \n");
+        System.out.print("Forkner ");
+    }
+}
