835e24c2faf458821b59a35a4fbfcdbff78c38e5
Phillip Forkner <phillip.forkner@gmail.com>
2025-04-21 12:27:02 -0700

    Mondday's class

--- src/AsciiFigure.java
+++ src/AsciiFigure.java
@@ -3,12 +3,19 @@ April 16, 2025
 Phillip Forkner
  */
 
-package CSBS; // You don't need to include this line
-
 public class AsciiFigure {
     public static final int SIZE = 5;
 
-    public static void main(String[] args) {
+    public static void writeChars(char c, int n) {
+        for (int i = 0; i < n; i++) System.out.print(c);
+    }
 
+    public static void main(String[] args) {
+        for (int row = 0; row < SIZE; row++) {
+            writeChars('/', (SIZE - row  - 1)*4);
+            writeChars('*', row*8);
+            writeChars('\\', (SIZE - row  - 1)*4);
+            System.out.println();
+        }
     }
 }
